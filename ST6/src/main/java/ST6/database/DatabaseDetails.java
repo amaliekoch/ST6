23383e235b50b55145646998b9700a5b71364f24
amaliekoch <akan18@student.aau.dk>
2021-04-22 13:59:35 +0200

    Config update

--- ST6/src/main/java/ST6/database/DatabaseDetails.java
+++ ST6/src/main/java/ST6/database/DatabaseDetails.java
@@ -12,6 +12,6 @@ public class DatabaseDetails {
     /**
      * Host for the Database connection
      */
-    final static protected String host = "jdbc:mysql://db.course.hst.aau.dk:3306/hst_2020_20gr5403?useUnicode=true&characterEncoding=utf8&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    final static protected String host = "jdbc:mysql://db.course.hst.aau.dk:3306/hst_2021_21gr6404";
 
 }
