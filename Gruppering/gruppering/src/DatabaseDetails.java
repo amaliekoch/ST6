4346fd7a14c4e557b30311cdb5ca01a38fcd0865
sbjorn10 <71770814+sbjorn10@users.noreply.github.com>
2021-05-14 08:26:10 +0200

    .

---
+++ Gruppering/gruppering/src/DatabaseDetails.java
@@ -0,0 +1,15 @@
+public class DatabaseDetails {
+    /**
+   * Username for the Database connection
+   */
+  final static protected String username = "hst_2021_21gr6404"; // brugernavn: hst_2021_21gr6404
+  /**
+   * Password for the Database connection
+   */
+  final static protected String password = "aiphaquaewahfaiyeequ"; // kodeord: aiphaquaewahfaiyeequ
+  /**
+   * Host for the Database connection
+   */
+  final static protected String host = "jdbc:mysql://db.course.hst.aau.dk:3306/hst_2021_21gr6404";
+
+}
